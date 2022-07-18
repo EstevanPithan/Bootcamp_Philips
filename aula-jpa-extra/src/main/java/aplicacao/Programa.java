a76ca16d5c0299fa8e8d38b54479b08624cc2f84
Estevan <estevanpithan@hotmail.com>
2022-07-18 12:43:00 -0300

    Atualizando o diretorio

--- aula-jpa-extra/src/main/java/aplicacao/Programa.java
+++ aula-jpa-extra/src/main/java/aplicacao/Programa.java
@@ -1,2 +1,49 @@
-package aplicacao;public class Programa {
+package aplicacao;
+
+import dominio.Pessoa;
+import org.w3c.dom.ls.LSOutput;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+public class Programa {
+
+    public static void main(String[] args) {
+        Pessoa p1 = new Pessoa(null, "Carlos", 123L);
+        Pessoa p2 = new Pessoa(null, "Joana", 456L);
+        Pessoa p3 = new Pessoa(null, "Ana", 789L);
+
+//        isso é para ter uma conexão com o banco de dados
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("AULA-JPA");
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+        em.persist(p1);
+        em.persist(p2);
+        em.persist(p3);
+        em.getTransaction().commit();
+
+        System.out.println(em.createQuery("Select cpf from Pessoa where cpf like :cpf").setParameter("cpf", 123L).getResultList());
+
+
+//        Pessoa p = em.getReference(Pessoa.class, 123);
+//                find(Pessoa.class., 123);
+//        em.getTransaction().begin();
+//        em.getTransaction().commit();
+//Caso não for uma simples leitura é necesário realizar uma transation
+//        em.getTransaction().begin();
+//        em.persist(p1);
+//        em.persist(p2);
+//        em.persist(p3);
+//        em.getTransaction().commit();
+        System.out.println();
+        System.out.println("Pronto!!!");
+        em.close();
+        emf.close();
+
+    }
+
+
 }
--- aula-jpa-extra/src/main/java/dominio/Pessoa.java
+++ aula-jpa-extra/src/main/java/dominio/Pessoa.java
@@ -1,2 +1,57 @@
-package PACKAGE_NAME;public class Pessoa {
+package dominio;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class Pessoa implements Serializable {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+    private Long cpf;
+
+    public Pessoa(){
+    }
+
+    public Pessoa(Integer id, String nome, Long cpf) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(Long cpf) {
+        this.cpf = cpf;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", cpf=" + cpf +
+                '}';
+    }
 }
